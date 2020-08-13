d5d49f37e331abdad5d7969e0d5b6d93353b04aa
FaxriFn <fakhrifn@code.edu.az>
2020-08-13 17:00:42 +0400

    NewData

--- OxuAz/OxuAz/Models/News.cs
+++ OxuAz/OxuAz/Models/News.cs
@@ -13,17 +13,27 @@ namespace OxuAz.Models
         public int Id { get; set; }
         public string Image { get; set; }
         [NotMapped]
+        [Display(Name = "Sekil")]
+
         public IFormFile  Photo { get; set; }
         [Required(ErrorMessage ="Basliq Yazin")]
         [StringLength(50, ErrorMessage = "Basliq 50 simvoldan artiq ola bilmez")]
+        [Display(Name = "Başlıq")]
 
         public string Title { get; set; }
         [Required(ErrorMessage = "Metn Yazin")]
         [StringLength(100,ErrorMessage ="Metn 100 simvoldan artiq ola bilmez")]
+        [Display(Name = "Mətn")]
 
         public string Content { get; set; }
+        [Display(Name = "Bəyənilib")]
+
         public int Like { get; set; }
+        [Display(Name = "Bəyənilməyib")]
+
         public int DisLike { get; set; }
+        [Display(Name = "Tarix")]
+
         public DateTime CreateAt { get; set; }
     }
 }
